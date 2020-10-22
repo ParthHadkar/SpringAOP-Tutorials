99b59d149cc9093be4b87858923c5ce92a6492e3
s-pc <s-pc@localhost>
2020-10-22 12:40:27 +0530

    P6 committed

---
+++ 06SpringAOPPointCutSharingAndOrderingAspects/src/main/java/com/spring_aop/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,20 @@
+package com.spring_aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+		
+	@Before("com.spring_aop.aspect.SharingPointCutExpressions.forDaoPackageWithOutSetterGetter()")
+	public void beforeAddAccountAdvice() {
+		
+		System.out.println("\n ====>>>>: Executing @Before advice on method");
+	}
+	
+}
